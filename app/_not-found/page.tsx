0b929120d502add1b5408e38a33c62ceb901d20d
DevTimmy <oyinloyematthew6@gmail.com>
2026-03-01 01:11:34 +0100

    Fix: Moved all the file into the main repo

---
+++ app/_not-found/page.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+export const dynamic = 'force-dynamic';
+
+const NotFoundPage = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1>Page Not Found.</h1>
+    </div>
+  );
+};
+
+export default NotFoundPage;
